81b0590eefbb6f4d676a07bc26b0b39df4968874
Bryan Bosire <bryanbosire@gmail.com>
2023-02-13 10:32:42 +0300

    added axios for sending the form data to the server for log in / authetication

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from '../api/axios'
 
 const LogIn = () => {
   const [formData, setFormData] = useState({
     email: "",
     password: ""
   });
+
+  const [errors, setErrors] = useState("");
+  const [authenticated, setAuthenticated] = useState(false);
 
   const handleChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -14,11 +18,23 @@ const LogIn = () => {
   const handleSubmit = e => {
     e.preventDefault();
     // Send the form data to the server for log in
+    axios.post('/auth/login', formData)
+      .then((response) => {
+        setAuthenticated(true);
+        localStorage.setItem('token', JSON.stringify(response.data.token))
+        localStorage.setItem('username', JSON.stringify(response.data.username))
+        localStorage.setItem('user_id', JSON.stringify(response.data.user_id))
+        localStorage.setItem('authenticated', JSON.stringify(true))        
+      }).catch(error => {
+        // Handle error
+        setErrors(error.response.data.error);
+    });      
   };
 
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "50vh" }}>
     <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      {errors && <p style={{color: "red"}}>{errors}</p>}
       <input
         type="email"
         name="email"
@@ -36,6 +52,7 @@ const LogIn = () => {
         required
       />
       <button type="submit">Log In</button>
+      {authenticated && <p style={{color: "green"}}>Successfully logged in!</p>}
       <p>
           {" "}
           Don't have an account?{" "}
