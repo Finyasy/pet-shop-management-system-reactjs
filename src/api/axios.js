c8fd1b4aa3df503b90018216f96495aa7de54929
Bryan Bosire <bryanbosire@gmail.com>
2023-02-13 10:35:53 +0300

    added axios for api communication

---
+++ src/api/axios.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+// import { json } from 'react-router-dom';
+
+// axios.defaults.baseURL = 'https://powerful-dusk-77185.herokuapp.com';
+axios.defaults.baseURL = 'http://localhost:5000';
+
+try {
+    axios.defaults.headers.common = {
+        Authorization: `bearer ${
+        localStorage.getItem('token') ? JSON.parse(localStorage.getItem('token') || undefined) : null
+    }`}
+} catch (error) { }
+
+export default axios
