bcb5a327e95a03204b220e620d02748b35c6a023
Bryan Bosire <bryanbosire@gmail.com>
2023-02-22 20:21:19 +0300

    removed the pet example and added pets prop

--- src/App.js
+++ src/App.js
@@ -6,51 +6,7 @@ import Header from "./Components/Header";
 import PetList from "./Components/PetList";
 
 
-const pets = [
-  {
-    id: 1,
-    name: 'Max',
-    dob: '01/01/2020',
-    sex: 'Male',
-    breed: 'German Shepherd',
-    color: 'Brown',
-    image: 'max.jpeg',
-    price: 1500
-  },
-  {
-    id: 2,
-    name: 'Bella',
-    dob: '02/02/2019',
-    sex: 'Female',
-    breed: 'Golden Retriever',
-    color: 'Yellow',
-    image: 'bella.jpeg',
-    price: 1000
-  },
-  {
-    id: 3,
-    name: 'Charlie',
-    dob: '03/03/2018',
-    sex: 'Male',
-    breed: 'Labrador Retriever',
-    color: 'Black',
-    image: 'charlie.jpeg',
-    price: 1200
-  },
-  {
-    id: 4,
-    name: 'Daisy',
-    dob: '04/04/2017',
-    sex: 'Female',
-    breed: 'Poodle',
-    color: 'White',
-    image: 'daisy.jpg',
-    price: 2000
-  }
-];
-
-const App = () => (
-
+const App = ({pets}) => (
     
   <Router>
     <Header />
