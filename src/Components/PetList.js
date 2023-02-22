bad59012d6d2c82371cf330312426ea3974f6653
Bryan Bosire <bryanbosire@gmail.com>
2023-02-22 20:19:02 +0300

    Added useEffect for fetching data from API server

--- src/Components/PetList.js
+++ src/Components/PetList.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Pet from './Pet'
+import axios from '../api/axios'
 
-const PetList = ({ pets }) => {
+const PetList = () => {
+
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/pets")
+      .then((response) => {
+        setPets(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
     return (
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         {pets.map((pet) => (
